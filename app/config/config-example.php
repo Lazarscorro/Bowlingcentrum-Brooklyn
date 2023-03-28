680d7ba9e59d0c794a3740e915488c63a86ff1a4
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-03-28 09:25:54 +0200

    Mvc template
    A nice begin template in mvc structure

---
+++ app/config/config-example.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'jou db naam hier');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'Jouw URL hier');
+
+define('SITENAME', 'sitenaam hier');
